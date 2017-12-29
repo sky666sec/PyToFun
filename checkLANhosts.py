32c12899ea6fd8a3bb19aa5df8dc38579f0ae017
Xie Yu Fei <2827340173@qq.com>
2017-12-29 16:26:33 +0800

    Update checkLANhosts.py
    Find the host that can be Ping through in the LAN

--- checkLANhosts.py
+++ checkLANhosts.py
@@ -1,20 +1,38 @@
 '''
-win下
+can run in win/linux
+
+	Find the host that can be Ping through in the LAN
+--------------------------------
 直接运行
 输入局域网网关
 使用的多进程，进程池为30
+
+auther:hyhmnn
+date:2017-12-29
 '''
 import os
+import platform
 from multiprocessing import Pool
 
-def ping_hostname(hostnumber,gateway):
+
+def ping_hostname(hostnumber, gateway):
 		hostname=gateway+str(hostnumber)
-		fd = os.popen('ping -n 1 {hostname}'.format(hostname=hostname))
+		comm, hint_info = winORlinux(hostname)
+		fd = os.popen(comm)
 		fdread=fd.read()
 		result = '{hostname} is {state}'
-		if '无法访问目标主机'not in fdread:
+		if not hint_info in fdread:
 			print(result.format(hostname=hostname,state='up'))
 
+def winORlinux(hostname):
+	sysstr = platform.system()
+	if sysstr == "Windows":
+		comm = 'ping -n 1 {hostn}'.format(hostn=hostname)
+		hint_info = '无法访问目标主机'
+	elif sysstr == "Linux":
+		comm = 'ping -c 1 {hostn}'.format(hostn=hostname)
+		hint_info = 'Unreachable'
+	return comm,hint_info
 
 if __name__ == '__main__':
 	gateways = input('input gateway.\nlike this (192.168.1.1):')
@@ -23,10 +41,10 @@ if __name__ == '__main__':
 		gateway += a+'.'
 
 	p = Pool(30)
-#30
-	for hostnumber in range(1,255):
+		#30
+	for hostnumber in range(1,256):
 		p.apply_async(ping_hostname,args=(hostnumber,gateway,))
 	print('wait ...')
 	p.close()
 	p.join()
-	print('done.')	
+	print('done.')
