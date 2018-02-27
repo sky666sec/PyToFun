677b4d3f2e05531c47821533450946331fa753e8
Xie Yu Fei <2827340173@qq.com>
2018-02-28 00:08:55 +0800

    Create MysqlHelper.py

---
+++ MysqlHelper.py
@@ -0,0 +1,63 @@
+# encoding=utf8
+import pymysql
+#pip3 install pymysql
+
+class MysqlHelper():
+    def __init__(self, host, port, db, user, passwd, charset="utf8"):
+        self.host = host
+        self.port = port
+        self.db = db
+        self.user = user
+        self.passwd = passwd
+        self.charset = charset
+
+    def connect(self):
+        self.conn = pymysql.connect(host=self.host, port=self.port, db=self.db,
+                                    user=self.user, password=self.passwd, charset=self.charset)
+        self.cursor = self.conn.cursor()
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
+
+    def get_one(self, sql):
+        result = ''
+        try:
+            self.connect()
+            self.cursor.execute(sql)
+            result = self.cursor.fetchone()
+            self.close()
+        except Exception as e:
+            print(e)
+        return result
+
+    def get_all(self, sql):
+        result = ''
+        try:
+            self.connect()
+            self.cursor.execute(sql)
+            result = self.cursor.fetchall()
+            self.close()
+            return result
+        except Exception as e:
+            print(e)
+        return result
+
+    def insert(self, sql, params):
+        return self.__edit(sql, params)
+
+    def update(self, sql, params):
+        return self.__edit(sql, params)
+
+    def delete(self, sql, params):
+        return self.__edit(sql, params)
+
+    def __edit(self, sql, params):
+        try:
+            self.connect()
+            conut = self.cursor.execute(sql, params)
+            self.conn.commit()
+            self.close()
+        except Exception as e:
+            print(e)
+        return conut
