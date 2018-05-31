497eb2f1693a7257af3af1e12224e93246ffbbdc
hyhmnn <2827340173@qq.com>
2018-05-31 12:05:28 +0800

    Update masscanPlug.py

--- masscanPlug.py
+++ masscanPlug.py
@@ -25,11 +25,17 @@ def main(hosts, ports, arguments=''):
 
 	4-use root run
 	'''
-	mas = masscan.PortScanner()
-	result = mas.scan(hosts, ports=ports, arguments=arguments)
+	try:
+		mas = masscan.PortScanner()
+		result = mas.scan(hosts, ports=ports, arguments=arguments)
+	except Exception, e:
+		if "network is unreachable" in e:
+			return json.dumps({'eroor':'network is unreachable'})
+		else:
+			return json.dumps({'eroor':e})
 	return json.dumps({'result':result['scan']}, separators=(',', ':'))
 
 if __name__ == '__main__':
-	# arguments='--max-rate 1000'
-	result = main("192.168.1.0/24", ports="80,8080,21,21")
+	arguments='--max-rate 100000'
+	result = main("192.168.1.0/24", ports="0-65535", arguments=arguments)
 	print result
